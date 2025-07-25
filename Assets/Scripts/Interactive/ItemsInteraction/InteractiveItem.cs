99881aee121ef0ad50d921cdcc1bd2207ef24ae8
Xdoxgg <m.zhuk007@gmail.com>
2025-07-25 19:36:19 +0300

    update interaction scripts

--- Assets/Scripts/Interactive/ItemsInteraction/InteractiveItem.cs
+++ Assets/Scripts/Interactive/ItemsInteraction/InteractiveItem.cs
@@ -2,8 +2,20 @@ using UnityEngine;
 
 namespace Interactive.ItemsInteraction
 {
-    public class InteractiveItem:MonoBehaviour
+    public class InteractiveItem:MonoBehaviour, IInteractive
     {
+        private string _name;
+        private GameObject _currentGameObject;
         
+        
+        public void Interact()
+        {
+            
+        }
+
+        public string GetDescription()
+        {
+            return $"Take {_name}";
+        }
     }
 }
