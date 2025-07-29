34fbb6e0b6086234a703b67fc1633903c1a78fe5
MindeRe <MindeRe@DESKTOP-CND4IL3>
2025-07-30 01:51:59 +0300

    added vzaimodeistvie with fisics

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -13,6 +13,8 @@ public class PlayerMovement : MonoBehaviour
     private int _speed = 5;
     [SerializeField]
     private int _jumpH = 5;
+    [SerializeField]
+    private int throwForce = 15;
     private int _gravity = -12;
 
     private void Awake()
@@ -23,28 +25,40 @@ public class PlayerMovement : MonoBehaviour
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
-        Debug.Log($"Move Input: {moveInput}");
+        //Debug.Log($"Move Input: {moveInput}");
     }
     public void OnJump(InputAction.CallbackContext context)
     {
-        Debug.Log("K");
         if(context.performed && _controller.isGrounded)
         {
             _velocity.y = _jumpH;
         }
     }
 
+    public void OnDash(InputAction.CallbackContext context)
+    {
+        _controller.Move(_dir * 2 *_speed * Time.deltaTime);
+    }
+
     private void Update()
     {
         if (_controller.isGrounded && _velocity.y < 0)
         {
-            Debug.Log("K1");
             _velocity.y = -6;
         }
+       
         _dir = moveInput.y * transform.forward + moveInput.x * transform.right;
         _velocity.y += _gravity * Time.deltaTime;
         _controller.Move(_dir * _speed * Time.deltaTime);
         _controller.Move(_velocity * Time.deltaTime);
        
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit != null && hit.rigidbody != null)
+        {
+            hit.rigidbody.AddForce(transform.forward * throwForce, ForceMode.Force);
+        }
     }
 }
